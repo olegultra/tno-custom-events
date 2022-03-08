d8743c6f1d36129bafda889dd3f4cf9fee67de64
Zakki0925224 <zakki0925224@gmail.com>
2022-03-08 16:19:20 +0900

    [Add]: Added super event text

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-function set_label(ui_name, pos_x, pos_y, font_size, is_center)
+function set_label(ui_name, pos_x, pos_y, font_size)
 {
     var label = document.getElementById(ui_name + "-label");
     var input = document.getElementById(ui_name + "-input");
     label.style.position = "absolute";
 
-    if (is_center)
-    {
-        // TODO
-        label.style.left = pos_x + "px";
-        label.style.top = pos_y + "px";
-    }
-    else
-    {
-        label.style.left = pos_x + "px";
-        label.style.top = pos_y + "px";
-    }
-
+    label.style.left = pos_x + "px";
+    label.style.top = pos_y + "px";
     label.style.fontSize = font_size + "px";
-
     label.innerText = input.value;
 }
 
@@ -31,10 +20,10 @@ function set_pic(ui_name)
     reader.readAsDataURL(input.files[0]);
 }
 
-function set_input_event(ui_name, pos_x, pos_y, font_size, is_center)
+function set_input_event(ui_name, pos_x, pos_y, font_size)
 {
     var input = document.getElementById(ui_name + "-input");
-    input.addEventListener("input", function() { set_label(ui_name, pos_x, pos_y, font_size, is_center); });
+    input.addEventListener("input", function() { set_label(ui_name, pos_x, pos_y, font_size); });
 }
 
 function set_input_value(ui_name, value)
@@ -62,6 +51,9 @@ function test_button_event()
     set_input_value("news-title", "ドイツ内戦");
     set_input_value("news-description", "アドルフ・ヒトラーの死後、帝国は大混乱に陥った。総統が正統な後継者を指名したのにも関わらず、帝国内における強力な派閥が自分たちの未来像で国家を導こうと互いに武装蜂起を開始した。ゲルマニアからの命令を無視して、それぞれの指導者に忠誠を誓う部隊によって国家は引き裂かれている。状況の深刻さはまだ分からないが、帝国は中央集権の完全な崩壊に直面しているか、あるいは既に崩壊しているのかもしれない。\n\n帝国の東方政府についてはまだ見通しが立っていないが、ゲルマニアとその他の地域からの連絡が途絶えた事によって、それらの地域にも運命の時が訪れるのではないかという憶測がなされている。しかし現在確かな事実は欧州の巨人が大陸を分断させたという事だ。");
     set_input_value("news-button", "確かなる帝国の終焉だ。");
+    set_input_value("super-event-title", "ドイツ内戦");
+    set_input_value("super-event-description", "国家の存立に比べて、自分の自我が重要でないことを自覚する\n必要がある。\n- アドルフ・ヒトラー");
+    set_input_value("super-event-button", "So it begins");
 
     document.getElementById("country-flag-pic").setAttribute("src", "./assets/sample/GER.png");
     document.getElementById("country-leader-pic").setAttribute("src", "./assets/sample/Portrait_Germany_Reichstag_Emergency_Council.png");
@@ -74,15 +66,19 @@ function test_button_event()
 window.onload = function()
 {
     document.getElementById("test-button").addEventListener("click", test_button_event);
-    set_input_event("country-name", 220, -5, 14, false);
-    set_input_event("faction-name", 220, 15, 14, false);
-    set_input_event("leader-name", 220, 35, 14, false);
-    set_input_event("party-name", 235, 72, 20, false);
-    set_input_event("ideology-name", 235, 97, 20, false);
-    set_input_event("next-election", 235, 118, 20, false);
-    set_input_event("national-focus-name", 260, 166, 20, true);
-    set_input_event("news-title", 250, 305, 15, true);
-    set_input_event("news-button", 250, 708, 12, true);
+    set_input_event("country-name", 220, -5, 14);
+    set_input_event("faction-name", 220, 15, 14);
+    set_input_event("leader-name", 220, 35, 14);
+    set_input_event("party-name", 235, 72, 20);
+    set_input_event("ideology-name", 235, 97, 20);
+    set_input_event("next-election", 235, 118, 20);
+    set_input_event("national-focus-name", 234, 163, 20);
+    set_input_event("news-title", 103, 295, 15);
+    set_input_event("news-description", 240, 340, 10);
+    set_input_event("news-button", 230, 679, 12);
+    set_input_event("super-event-title", 530, 0, 20);
+    set_input_event("super-event-description", 560, 360, 19);
+    set_input_event("super-event-button", 720, 548, 19);
     set_pic_event("country-flag");
     set_pic_event("country-leader");
     set_pic_event("ideology-icon");
